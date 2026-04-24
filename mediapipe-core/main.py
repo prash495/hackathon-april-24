510a0fe6ad9a4a3852882cd5b4513a727050f7ea
prash495 <261973235+prash495@users.noreply.github.com>
2026-04-24 13:41:31 -0700

    added video input capture to mediapipe

--- mediapipe-core/main.py
+++ mediapipe-core/main.py
@@ -1,6 +1,6 @@
+import cv2
 import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
+import time
 
 model_path = 'mediapipe-core/face_landmarker_v2_with_blendshapes.task'
 
@@ -10,7 +10,6 @@ FaceLandmarkerOptions = mp.tasks.vision.FaceLandmarkerOptions
 FaceLandmarkerResult = mp.tasks.vision.FaceLandmarkerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
-# Create a face landmarker instance with the live stream mode:
 def print_result(result: FaceLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
     print('face landmarker result: {}'.format(result))
 
@@ -19,7 +18,20 @@ options = FaceLandmarkerOptions(
     running_mode=VisionRunningMode.LIVE_STREAM,
     result_callback=print_result)
 
+cap = cv2.VideoCapture(0)  # 0 = default camera
+
 with FaceLandmarker.create_from_options(options) as landmarker:
-  # The landmarker is initialized. Use it here.
-  # ...
-    
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+        landmarker.detect_async(mp_image, int(time.time() * 1000))
+
+        cv2.imshow('Face Landmarker', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
