2ec102125fd5e318dc4f056c90fa2c64583e86be
user <user@domain.ext>
2026-04-24 20:44:08 -0700

    minor

--- apps/backend/main.py
+++ apps/backend/main.py
@@ -564,4 +564,4 @@ def cleanup_proctors():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_config=None)
